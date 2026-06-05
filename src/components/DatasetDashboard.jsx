18738f76dcf5dd6c84c35ee0261e7a9b5a1e2da8
Joe Kenyon <joekenyon72@gmail.com>
2026-06-05 15:39:47 +0100

    okay now it goes to 404 if you type in rubbish

--- src/components/DatasetDashboard.jsx
+++ src/components/DatasetDashboard.jsx
@@ -1,19 +1,22 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import { ONS_DATASETS } from '../data/ONS_DATASETS';
-import { getDatasetMetadata, getObservations } from '../services/onsService';
 import styles from './DatasetDashboard.module.css';
 
 export default function DatasetDashboard() 
 {
   const { datasetId } = useParams();
   const currentDataset = ONS_DATASETS.find(d => d.id === datasetId);
-  if (!currentDataset) 
+  
+  if (currentDataset)
   {
     return (
       <>
         <h1 className={styles.sectionTitle}>{currentDataset.name}</h1>
       </>
     );
+  }
+  else
+  {
+    return none;
   }
 }
